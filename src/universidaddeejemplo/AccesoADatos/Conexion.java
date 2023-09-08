ad37c42bec2f9cc1523bec14fb2a1ba3b7504172
gianfpino <gianfpino@gmail.com>
2023-09-08 17:34:23 -0300

    Conexion

--- src/universidaddeejemplo/AccesoADatos/Conexion.java
+++ src/universidaddeejemplo/AccesoADatos/Conexion.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidaddeejemplo.AccesoADatos;
 
-/**
- *
- * @author lau-d
- */
+
 public class Conexion {
-    
+    private static final String URL="jdbc:mariadb://localhost/";
+    private static final String DB="universidadulp";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
 }
