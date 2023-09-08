158862828c47a13eb533376557a49c70cf47caad
gianfpino <gianfpino@gmail.com>
2023-09-08 17:45:25 -0300

    clase inscripcion (creacion)

---
+++ src/univesidadejemplo/Entidades/Inscripcion.java
@@ -0,0 +1,10 @@
+
+package univesidadejemplo.Entidades;
+
+
+public class Inscripcion {
+    private int idInscripcion;
+    private Alumno alumno;
+    private Materia materia;
+    private double nota;
+}
