a59db47f117959f819b3a0b0598f0f479a65c13e
gianfpino <gianfpino@gmail.com>
2023-09-11 17:48:40 -0300

    Atributo y constructor en MateriaData

--- src/ulp/AccesoADatos/MateriaData.java
+++ src/ulp/AccesoADatos/MateriaData.java
@@ -1,7 +1,14 @@
 
 package ulp.AccesoADatos;
 
+import java.sql.Connection;
+
 
 public class MateriaData {
-    
+    private Connection con = null;
+	
+	public MateriaData(){
+
+	con = Conexion.getConexion();
+        }
 }
