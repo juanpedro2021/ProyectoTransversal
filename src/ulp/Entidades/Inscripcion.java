af9bf04716406952f03b1578a6514d5c8bd4d9f2
juanpedro2021 <juanpedro103.jp@gmail.com>
2023-09-12 16:44:25 -0300

    clase inscripcion

--- src/ulp/Entidades/Inscripcion.java
+++ src/ulp/Entidades/Inscripcion.java
@@ -7,4 +7,48 @@ public class Inscripcion {
     private Alumno alumno;
     private Materia materia;
     private double nota;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion( Alumno alumno, Materia materia, double nota) {
+        
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public int getIdInscripcion() {
+        return idInscripcion;
+    }
+
+    public void setIdInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    
+    
 }
