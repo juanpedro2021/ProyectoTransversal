533f27c237d709f351922a08e62ca40fe3980be2
pableKo <141676090+pableKo@users.noreply.github.com>
2023-09-12 14:08:37 -0300

    cree guardarMateria

--- src/ulp/AccesoADatos/MateriaData.java
+++ src/ulp/AccesoADatos/MateriaData.java
@@ -2,6 +2,15 @@
 package ulp.AccesoADatos;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import ulp.Entidades.Materia;
 
 
 public class MateriaData {
@@ -11,4 +20,39 @@ public class MateriaData {
 
 	con = Conexion.getConexion();
         }
+
+
+public class MateriaData {
+	private Connection con = null;
+	
+	public MateriaData(){
+
+	con = Conexion.getConexion();
+        }
+
+public void guardarMateria (Materia materia) {
+	
+	String sql = "INSERT INTO materia (nombre, añoMateria, activo) VALUES (?, ?, ?)";
+	try {
+		PreparedStatement ps = con.prepareStatement (sql, Statement.RETURN_GENERATED_KEYS);
+		ps.setString(1, materia.getNombre());
+		ps.setInt(2, materia.getAñoMateria());
+		ps.setBoolean(3, materia.isActivo());
+		ps.executeUpdate();
+		ResultSet rs = ps.getGeneratedKeys();
+
+		if (rs.next()){
+			materia.setIdMateria(rs.getInt("idMateria"));
+		JOptionPane.showMessageDialog(null, "Materia añadida con éxito");
+		}
+		ps.close();
+		} catch (SQLException ex){
+		JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Materia"+ex.getMessage());
+		}
+		}
 }
+
+}
+
+
+
--- src/ulp/Entidades/Materia.java
+++ src/ulp/Entidades/Materia.java
@@ -7,4 +7,20 @@ public class Materia {
     private String nombre;
     private int añoMateria;
     private boolean activo;
+
+    public String getNombre() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getAñoMateria() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean isActivo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setIdMateria(int aInt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
