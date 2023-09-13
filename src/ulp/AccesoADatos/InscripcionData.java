016c5f7c0a914ad8bebba599fde9cb886275e689
gianfpino <gianfpino@gmail.com>
2023-09-13 16:45:46 -0300

    actualizarNota (InscripcionData)

--- src/ulp/AccesoADatos/InscripcionData.java
+++ src/ulp/AccesoADatos/InscripcionData.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JOptionPane;
 
@@ -49,6 +51,25 @@ public void guardarInscripcion(Inscripcion ins){
         JOptionPane.showMessageDialog(null, "Error al acceder a la tabla inscripcion"+ex.getMessage()); 
 
          }
+}
+public void actualizarNota(int idAlumno, int idMateria, double nota){
+    
+    String sql = "UPDATE inscripcion SET nota = ? WHERE idAlumno=? AND idMateria = ? ";
+            try {
+                PreparedStatement ps=con.prepareStatement(sql);
+                ps.setDouble(1, nota);
+                ps.setInt(2, idAlumno);
+                ps.setInt(3, idMateria);
+                int filas=ps.executeUpdate();
+                
+                if(filas>0){
+                   JOptionPane.showMessageDialog(null, "Nota actualizada");
+                }
+                ps.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(InscripcionData.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    
 }
  
          
