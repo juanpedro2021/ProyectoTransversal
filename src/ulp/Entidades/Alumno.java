053e8f15c814c3e8d806bab55111072bd1ecab94
gianfpino <gianfpino@gmail.com>
2023-09-15 16:46:42 -0300

    actualizacion materiaData /Alumno

--- src/ulp/Entidades/Alumno.java
+++ src/ulp/Entidades/Alumno.java
@@ -6,13 +6,15 @@ import java.time.LocalDate;
 
 public class Alumno {
     private int idAlumno;
+    private int dni;
     private String apellido;
     private String nombre;
     private LocalDate fechaNac;
     private boolean activo;
 
-    public Alumno(int idAlumno, String apellido, String nombre, LocalDate fechaNac, boolean activo) {
+    public Alumno(int idAlumno, int dni, String apellido, String nombre, LocalDate fechaNac, boolean activo) {
         this.idAlumno = idAlumno;
+        this.dni= dni;
         this.apellido = apellido;
         this.nombre = nombre;
         this.fechaNac = fechaNac;
@@ -68,6 +70,15 @@ public class Alumno {
     public void setActivo(boolean activo) {
         this.activo = activo;
     }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+    
 
     @Override
     public String toString() {
